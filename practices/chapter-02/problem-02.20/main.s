a49e7a5d3a07a9957a90fcad3c923981b45c65fc
oportunitas <taibizzat165@gmail.com>
2026-07-08 10:55:47 +0700

    ✨ problem 02.20

---
+++ practices/chapter-02/problem-02.20/main.s
@@ -0,0 +1,18 @@
+/*
+    explain how equation 2.5 applies to the entries in the table you generated when solving problem 2.19
+
+        equation 2.5 (t2u_w(x)):
+            x + 2^w, x < 0,
+            x      , x >= 0,
+
+        [x]     [t2u_4(x)]
+        -8      -8 + 16 = 8
+        -3      -3 + 16 = 13
+        -2      -2 + 16 = 14
+        -1      -1 + 16 = 15
+        0       0       = 0
+        5       5       = 5
+
+        first four entries are negative, so t2u_4(x) is equal to x + 2^4
+        last two entries are positive, so t2u4(x) is equal to x
+*/

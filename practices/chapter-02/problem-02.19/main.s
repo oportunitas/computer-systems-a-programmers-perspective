1b9cf67f0092b6fc92c9166ea844fd8bc164fc3f
oportunitas <taibizzat165@gmail.com>
2026-07-08 10:47:35 +0700

    ✨ problem 02.19

---
+++ practices/chapter-02/problem-02.19/main.s
@@ -0,0 +1,13 @@
+/*
+    using the table you filled in when solving problem 2.17, fill in the following table describing the function t2u_4():
+
+    [x]     t2b_4(x)    b2u_4(x)    [t2u_4(x)]
+    -8      1000        8           8
+    -3      1101        13          13
+    -2      1110        14          14
+    -1      1111        15          15
+    0       0000        0           0
+    5       0101        5           5
+
+    check using below program
+*/

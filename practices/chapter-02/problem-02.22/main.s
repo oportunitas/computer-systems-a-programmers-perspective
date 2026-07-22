26b68ee32ae3e8cf758a2597012534acde3af20b
oportunitas <taibizzat165@gmail.com>
2026-07-22 11:20:14 +0700

    ✨ problem 02.22

---
+++ practices/chapter-02/problem-02.22/main.s
@@ -0,0 +1,9 @@
+/*
+    Show that each of the following bit vectors is a two's-complement representation of –5 by applying Equation 2.3:
+
+    [1011]      : -2^3 + 2^1 + 2^0              = -8 + 2 + 1            = -5
+    [11011]     : -2^4 + 2^3 + 2^1 + 2^0        = -16 + 8 + 2 + 1       = -5
+    [111011]    : -2^5 + 2^4 + 2^3 + 2^1 + 2^0  = -32 + 16 + 8 + 2 + 1  = -5
+
+    Observe that the second and third bit vectors can be derived from the first by sign extension.
+*/

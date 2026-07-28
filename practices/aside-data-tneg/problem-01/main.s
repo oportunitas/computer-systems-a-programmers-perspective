ca017ddc416311a2b4c14183504326d7dda00a0f
oportunitas <taibizzat165@gmail.com>
2026-07-28 09:10:45 +0700

    ✨ data:tneg problem 01

---
+++ practices/aside-data-tneg/problem-01/main.s
@@ -0,0 +1,10 @@
+/*
+    Fill in the following table showing the effects of complementing and incrementing several five-bit vectors in the style of Figure 1. Show both the bit vectors and the numeric values.
+
+        x               ˜x              incr (˜~x)
+        [01101] (13)    [10010] (-14)   [10011] (-13)
+        [01110] (14)    [10001] (-15)   [10010] (-14)
+        [11000] (-8)    [00111] (7)     [01000] (8)
+        [11111] (-1)    [00000] (0)     [00001] (1)
+        [10000] (-16)   [01111] (15)    [10000] (-16)
+*/

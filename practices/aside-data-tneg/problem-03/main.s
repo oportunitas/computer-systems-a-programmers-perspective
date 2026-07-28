9b4e94ac493ca9174a67cb26101f57fd52dc4eb8
oportunitas <taibizzat165@gmail.com>
2026-07-28 09:21:03 +0700

    ✨ data:tneg problem 03

---
+++ practices/aside-data-tneg/problem-03/main.s
@@ -0,0 +1,10 @@
+/*
+    Show how the bit-level negation procedure applies to the examples of Problem 1. That is, 1) determine the bit position k of the rightmost 1, and 2) apply the rule of complementing the bits to the left of position k.
+
+        x               incr (˜~x)
+        [0110|1] (13)    [1001|1] (-13)
+        [011|10] (14)    [100|10] (-14)
+        [1|1000] (-8)    [0|1000] (8)
+        [1111|1] (-1)    [0000|1] (1)
+        [|10000] (-16)   [|10000] (-16 
+*/

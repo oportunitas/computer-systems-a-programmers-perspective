e14b21be663141661e6af08b2d5d8fb62e8e047a
oportunitas <taibizzat165@gmail.com>
2026-07-28 09:17:59 +0700

    ✨ data:tneg problem 02

---
+++ practices/aside-data-tneg/problem-02/main.s
@@ -0,0 +1,5 @@
+/*
+    Show that first decrementing and then complementing is equivalent to complementing and then incrementing. That is, for any signed value x, the C expressions -x, ˜x+1, and ˜(x-1) yield identical results. What mathematical properties of two’s-complement addition does your derivation rely on?
+
+        under the agreed properties of two's complement addition, ˜x resulting in (-x - 1) also means (under commutativity) that ˜(x - 1) results in -x
+*/

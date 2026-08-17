60bb0fafca0fc931e9c9a6664ccc84f8889972be
oportunitas <taibizzat165@gmail.com>
2026-08-17 15:41:06 +0700

    ✨ problem 02.45

---
+++ practices/chapter-02/problem-02.45/main.s
@@ -0,0 +1,13 @@
+/*
+    Fill in the missing information in the following table:
+
+    Fractional value	Binary representation	Decimal representation
+    1/8                 0.001                   0.125
+    3/4                 0.11                    0.75
+    5/16                0.0101                  0.3125
+    25/16               1.1001                  1.5625
+    2 + 11/16           10.1011                 2.6875
+    1 + 1/8             1.001                   1.125
+    5 + 7/8             101.111                 5.875
+    3 + 3/16            11.0011                 3.1875
+*/
